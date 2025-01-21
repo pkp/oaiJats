5bae28f401959114df3ec9c3f7e9c9a5746eeef1
Jonas Raoni Soares da Silva <github@raoni.org>
2025-01-21 16:15:30 +0300

    Formatting

--- OAIJatsSettingsForm.php
+++ OAIJatsSettingsForm.php
@@ -8,6 +8,7 @@
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class OAIJatsSettingsForm
+ *
  * @ingroup plugins_generic_webFeed
  *
  * @brief Form for managers to modify web feeds plugin settings
@@ -16,67 +17,76 @@
 namespace APP\plugins\oaiMetadataFormats\oaiJats;
 
 use APP\template\TemplateManager;
-use PKP\form\validation\FormValidatorPost;
 use PKP\form\validation\FormValidatorCSRF;
+use PKP\form\validation\FormValidatorPost;
 
-class OAIJatsSettingsForm extends \PKP\form\Form {
+class OAIJatsSettingsForm extends \PKP\form\Form
+{
+    /** @var int Associated context ID */
+    private $_contextId;
 
-	/** @var int Associated context ID */
-	private $_contextId;
+    /** @var WebFeedPlugin Web feed plugin */
+    private $_plugin;
 
-	/** @var WebFeedPlugin Web feed plugin */
-	private $_plugin;
+    /**
+     * Constructor
+     *
+     * @param $plugin WebFeedPlugin Web feed plugin
+     * @param $contextId int Context ID
+     */
+    public function __construct($plugin, $contextId)
+    {
+        $this->_contextId = $contextId;
+        $this->_plugin = $plugin;
 
-	/**
-	 * Constructor
-	 * @param $plugin WebFeedPlugin Web feed plugin
-	 * @param $contextId int Context ID
-	 */
-	function __construct($plugin, $contextId) {
-		$this->_contextId = $contextId;
-		$this->_plugin = $plugin;
+        parent::__construct($plugin->getTemplateResource('settingsForm.tpl'));
+        $this->addCheck(new FormValidatorPost($this));
+        $this->addCheck(new FormValidatorCSRF($this));
+    }
 
-		parent::__construct($plugin->getTemplateResource('settingsForm.tpl'));
-		$this->addCheck(new FormValidatorPost($this));
-		$this->addCheck(new FormValidatorCSRF($this));
-	}
+    /**
+     * Initialize form data.
+     */
+    public function initData()
+    {
+        $contextId = $this->_contextId;
+        $plugin = $this->_plugin;
 
-	/**
-	 * Initialize form data.
-	 */
-	function initData() {
-		$contextId = $this->_contextId;
-		$plugin = $this->_plugin;
+        $this->setData('forceJatsTemplate', $plugin->getSetting($contextId, 'forceJatsTemplate'));
+    }
 
-		$this->setData('forceJatsTemplate', $plugin->getSetting($contextId, 'forceJatsTemplate'));
-	}
+    /**
+     * Assign form data to user-submitted data.
+     */
+    public function readInputData()
+    {
+        $this->readUserVars(['forceJatsTemplate']);
+    }
 
-	/**
-	 * Assign form data to user-submitted data.
-	 */
-	function readInputData() {
-		$this->readUserVars(['forceJatsTemplate']);
-	}
+    /**
+     * Fetch the form.
+     *
+     * @copydoc Form::fetch()
+     *
+     * @param null|mixed $template
+     */
+    public function fetch($request, $template = null, $display = false)
+    {
+        $templateMgr = TemplateManager::getManager($request);
+        $templateMgr->assign('pluginName', $this->_plugin->getName());
+        return parent::fetch($request, $template, $display);
+    }
 
-	/**
-	 * Fetch the form.
-	 * @copydoc Form::fetch()
-	 */
-	function fetch($request, $template = null, $display = false) {
-		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign('pluginName', $this->_plugin->getName());
-		return parent::fetch($request, $template, $display);
-	}
+    /**
+     * @copydoc Form::execute()
+     */
+    public function execute(...$functionArgs)
+    {
+        $plugin = $this->_plugin;
+        $contextId = $this->_contextId;
 
-	/**
-	 * @copydoc Form::execute()
-	 */
-	function execute(...$functionArgs) {
-		$plugin = $this->_plugin;
-		$contextId = $this->_contextId;
+        $plugin->updateSetting($contextId, 'forceJatsTemplate', $this->getData('forceJatsTemplate'));
 
-		$plugin->updateSetting($contextId, 'forceJatsTemplate', $this->getData('forceJatsTemplate'));
-
-		parent::execute(...$functionArgs);
-	}
+        parent::execute(...$functionArgs);
+    }
 }
--- OAIMetadataFormatPlugin_JATS.php
+++ OAIMetadataFormatPlugin_JATS.php
@@ -8,6 +8,7 @@
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * @class OAIMetadataFormatPlugin_JATS
+ *
  * @see OAI
  *
  * @brief JATS XML format plugin for OAI.
@@ -17,131 +18,154 @@ namespace APP\plugins\oaiMetadataFormats\oaiJats;
 
 use APP\core\Application;
 use APP\notification\NotificationManager;
-use PKP\plugins\OAIMetadataFormatPlugin;
 use PKP\core\JSONMessage;
-use PKP\linkAction\request\AjaxModal;
 use PKP\linkAction\LinkAction;
+use PKP\linkAction\request\AjaxModal;
+use PKP\plugins\OAIMetadataFormatPlugin;
 
-class OAIMetadataFormatPlugin_JATS extends OAIMetadataFormatPlugin {
-	/**
-	 * Get the name of this plugin. The name must be unique within
-	 * its category.
-	 * @return String name of plugin
-	 */
-	function getName() {
-		return 'OAIMetadataFormatPlugin_JATS';
-	}
+class OAIMetadataFormatPlugin_JATS extends OAIMetadataFormatPlugin
+{
+    /**
+     * Get the name of this plugin. The name must be unique within
+     * its category.
+     *
+     * @return String name of plugin
+     */
+    public function getName()
+    {
+        return 'OAIMetadataFormatPlugin_JATS';
+    }
 
-	/**
-	 * @copydoc Plugin::getDisplayName()
-	 */
-	function getDisplayName() {
-		return __('plugins.oaiMetadata.jats.displayName');
-	}
+    /**
+     * @copydoc Plugin::getDisplayName()
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.oaiMetadata.jats.displayName');
+    }
 
-	/**
-	 * @copydoc Plugin::getDescription()
-	 */
-	function getDescription() {
-		return __('plugins.oaiMetadata.jats.description');
-	}
+    /**
+     * @copydoc Plugin::getDescription()
+     */
+    public function getDescription()
+    {
+        return __('plugins.oaiMetadata.jats.description');
+    }
 
-	/**
-	 * Determine whether the plugin can be disabled.
-	 * @return boolean
-	 */
-	function getCanDisable() {
-		return true;
-	}
+    /**
+     * Determine whether the plugin can be disabled.
+     *
+     * @return boolean
+     */
+    public function getCanDisable()
+    {
+        return true;
+    }
 
-	/**
-	 * Determine whether the plugin can be enabled.
-	 * @return boolean
-	 */
-	function getCanEnable() {
-		return true;
-	}
+    /**
+     * Determine whether the plugin can be enabled.
+     *
+     * @return boolean
+     */
+    public function getCanEnable()
+    {
+        return true;
+    }
 
-	/**
-	 * Determine whether the plugin is enabled.
-	 * @return boolean
-	 */
-	function getEnabled() {
-		$request = Application::get()->getRequest();
-		if (!$request) return false;
-		$context = $request->getContext();
-		if (!$context) return false;
-		return $this->getSetting($context->getId(), 'enabled');
-	}
+    /**
+     * Determine whether the plugin is enabled.
+     *
+     * @return boolean
+     */
+    public function getEnabled()
+    {
+        $request = Application::get()->getRequest();
+        if (!$request) {
+            return false;
+        }
+        $context = $request->getContext();
+        if (!$context) {
+            return false;
+        }
+        return $this->getSetting($context->getId(), 'enabled');
+    }
 
-	/**
-	 * Set whether the plugin is enabled.
-	 * @param $enabled boolean
-	 */
-	function setEnabled($enabled) {
-		$request = Application::get()->getRequest();
-		$context = $request->getContext();
-		$this->updateSetting($context->getId(), 'enabled', $enabled, 'bool');
-	}
+    /**
+     * Set whether the plugin is enabled.
+     *
+     * @param $enabled boolean
+     */
+    public function setEnabled($enabled)
+    {
+        $request = Application::get()->getRequest();
+        $context = $request->getContext();
+        $this->updateSetting($context->getId(), 'enabled', $enabled, 'bool');
+    }
 
-	/**
-	 * @copydoc Plugin::getActions()
-	 */
-	public function getActions($request, $verb) {
-		$router = $request->getRouter();
-		return array_merge(
-			$this->getEnabled()?[
-				new LinkAction(
-					'settings',
-					new AjaxModal(
-						$router->url($request, null, null, 'manage', null, array('verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'oaiMetadataFormats')),
-						$this->getDisplayName()
-					),
-					__('manager.plugins.settings'),
-					null
-				),
-			]:[],
-			parent::getActions($request, $verb)
-		);
-	}
+    /**
+     * @copydoc Plugin::getActions()
+     */
+    public function getActions($request, $verb)
+    {
+        $router = $request->getRouter();
+        return array_merge(
+            $this->getEnabled() ? [
+                new LinkAction(
+                    'settings',
+                    new AjaxModal(
+                        $router->url($request, null, null, 'manage', null, ['verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'oaiMetadataFormats']),
+                        $this->getDisplayName()
+                    ),
+                    __('manager.plugins.settings'),
+                    null
+                ),
+            ] : [],
+            parent::getActions($request, $verb)
+        );
+    }
 
-	/**
-	 * @copydoc Plugin::manage()
-	 */
-	public function manage($args, $request) {
-		switch ($request->getUserVar('verb')) {
-			case 'settings':
-				$form = new OAIJatsSettingsForm($this, $request->getContext()->getId());
+    /**
+     * @copydoc Plugin::manage()
+     */
+    public function manage($args, $request)
+    {
+        switch ($request->getUserVar('verb')) {
+            case 'settings':
+                $form = new OAIJatsSettingsForm($this, $request->getContext()->getId());
 
-				if ($request->getUserVar('save')) {
-					$form->readInputData();
-					if ($form->validate()) {
-						$form->execute();
-						$notificationManager = new NotificationManager();
-						$notificationManager->createTrivialNotification($request->getUser()->getId());
-						return new JSONMessage(true);
-					}
-				} else {
-					$form->initData();
-				}
-				return new JSONMessage(true, $form->fetch($request));
-		}
-		return parent::manage($args, $request);
-	}
+                if ($request->getUserVar('save')) {
+                    $form->readInputData();
+                    if ($form->validate()) {
+                        $form->execute();
+                        $notificationManager = new NotificationManager();
+                        $notificationManager->createTrivialNotification($request->getUser()->getId());
+                        return new JSONMessage(true);
+                    }
+                } else {
+                    $form->initData();
+                }
+                return new JSONMessage(true, $form->fetch($request));
+        }
+        return parent::manage($args, $request);
+    }
 
-	function getFormatClass() {
-		return '\APP\plugins\oaiMetadataFormats\oaiJats\OAIMetadataFormat_JATS';
-	}
+    public function getFormatClass()
+    {
+        return '\APP\plugins\oaiMetadataFormats\oaiJats\OAIMetadataFormat_JATS';
+    }
 
-	static function getMetadataPrefix() {
-		return 'jats';
-	}
+    public static function getMetadataPrefix()
+    {
+        return 'jats';
+    }
 
-	static function getSchema() {
-		return 'https://jats.nlm.nih.gov/publishing/0.4/xsd/JATS-journalpublishing0.xsd';
-	}
+    public static function getSchema()
+    {
+        return 'https://jats.nlm.nih.gov/publishing/0.4/xsd/JATS-journalpublishing0.xsd';
+    }
 
-	static function getNamespace() {
-		return 'http://jats.nlm.nih.gov';
-	}
+    public static function getNamespace()
+    {
+        return 'http://jats.nlm.nih.gov';
+    }
 }
